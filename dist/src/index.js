935bcac71e029da4f5757e9c2bb3c74fc362afb6
lindingfeng <lindingfeng@huishoubao.com.cn>
2023-05-19 17:39:48 +0800

    build: add new dependencies

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.decrypt = exports.encrypt = void 0;
+var Buffer = require('safe-buffer').Buffer;
 var _a = require('browserify-cipher/browser.js'), createCipheriv = _a.createCipheriv, createDecipheriv = _a.createDecipheriv;
 var randomBytes = require('randombytes');
 function encrypt(key, input) {
@@ -20,8 +21,7 @@ function decrypt(key, input) {
     var text = inputBuffer.slice(32);
     var decipher = createDecipheriv('aes-256-gcm', keyBuffer, iv);
     decipher.setAuthTag(tag);
-    var decrypted = decipher.update(text) + decipher.final('utf8');
-    return decrypted;
+    return decipher.update(text) + decipher.final('utf8');
 }
 exports.decrypt = decrypt;
 //# sourceMappingURL=index.js.map
