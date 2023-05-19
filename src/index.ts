842d014ecad476de5e776ee8a85e4f5f07d545fa
lindingfeng <lindingfeng@huishoubao.com.cn>
2023-05-19 17:38:22 +0800

    perf: add new dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // const randomBytes = require('crypto').randomBytes
 // const createCipheriv = require('crypto').createCipheriv
 // const createDecipheriv = require('crypto').createDecipheriv
+const Buffer = require('safe-buffer').Buffer;
 const { createCipheriv, createDecipheriv } = require('browserify-cipher/browser.js');
 const randomBytes = require('randombytes');
 
@@ -8,11 +9,7 @@ const randomBytes = require('randombytes');
 export function encrypt(key: string, input: string): string {
   const keyBuffer = Buffer.from(key)
   const iv = randomBytes(16)
-  // const keyBuffer = key
-  // const iv = key
-
   const cipher = createCipheriv('aes-256-gcm', keyBuffer, iv)
-
   const encrypted = Buffer.concat([cipher.update(input, 'utf8'), cipher.final()])
   const tag = cipher.getAuthTag()
 
@@ -22,15 +19,12 @@ export function encrypt(key: string, input: string): string {
 export function decrypt(key: string, input: string): string {
   const keyBuffer = Buffer.from(key)
   const inputBuffer = Buffer.from(input, 'base64')
-
   const iv = inputBuffer.slice(0, 16)
   const tag = inputBuffer.slice(16, 32)
   const text = inputBuffer.slice(32)
-
   const decipher = createDecipheriv('aes-256-gcm', keyBuffer, iv)
+
   decipher.setAuthTag(tag)
 
-  const decrypted = decipher.update(text) + decipher.final('utf8')
-
-  return decrypted
+  return decipher.update(text) + decipher.final('utf8')
 }
